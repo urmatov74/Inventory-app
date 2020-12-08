2e726c897594f1b5f32ebe47370d5e552ce5b91d
urmatov74 <a.urmatov7@gmail.com>
2020-12-08 17:43:41 +0600

    web-page-of-project

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <Header />
+    <Cards v-bind:cards="cards" />
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header';
+  import Cards from './components/Cards';
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Cards
+    },
+    data() {
+      return {
+        items: [{
+            id: 1,
+            title: "one"
+          },
+          {
+            id: 2,
+            title: "two"
+          },
+          {
+            id: 3,
+            title: "three"
+          }
+        ],
+        cards: [{
+            id: 1,
+            title: "first"
+          },
+          {
+            id: 2,
+            title: "second"
+          },
+          {
+            id: 3,
+            title: "third"
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Cards.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div class="list list-inline" v-bind:key="card.id" v-for="card in cards">
+            <CardsItem v-bind:card="card" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import CardsItem from './CardsItem.vue'
+    export default {
+        name: "Cards",
+        components: {
+            CardsItem
+        },
+        props: ["cards"]
+    }
+</script>
+
+<style scoped>
+    .container {
+        padding: 20px;
+        position: relative;
+        display: flex;
+    }
+    .list {
+        width: 200px;
+        background-color: #e3e3e3;
+        border-radius: 4px;
+        padding: 10px;
+        border: 1px solid #999;
+        vertical-align: top;
+        position: relative;
+    }
+</style>
+
