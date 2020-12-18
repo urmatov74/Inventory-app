a9a0d5465bbd838631f883eff0d6340e1763d5d6
urmatov74 <62984709+urmatov74@users.noreply.github.com>
2020-12-18 14:20:24 +0600

    Revert "Card fixes"

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,16 +1,16 @@
 <template>
     <v-row class="cards-container">
-        <v-col v-for="(card, id) in cards" :key="id" :cols="card.flex" fluid fill-height text-right>
+        <v-col v-for="(card,id) in cards" :key="id" :cols="card.flex" fluid fill-height text-right>
             <v-card class="cards">
                 <v-card-title>
                     {{card.title}}
                 </v-card-title>
-                <draggable class="list-group kanban-column" :items="items" group="tasks">
-                    <v-card class="card-items" fluid v-for="(item,id) in items" :key="id" height="150">
+                <draggable class="list-group kanban-column" :items="items" group="tasks" @change="log">
+                    <v-card class="card-items" fluid v-for="(item,id) in card.items" :key="id" height="150">
                         <v-card-title>
-                            {{item.name}}
+                            {{item.title}}
                             <v-spacer></v-spacer>
-                            <v-btn @click="deleteItem(item.id)" elevation="2" icon color="black" dark>
+                            <v-btn elevation="2" icon color="black" dark>
                                 <v-icon>mdi-delete</v-icon>
                             </v-btn>
                         </v-card-title>
@@ -28,7 +28,7 @@
                         mdi-plus
                     </v-icon>
                 </v-btn>
-                <v-dialog v-model="dialog2" :retain-focus="false" max-width="500px">
+                <v-dialog v-model="dialog2" max-width="500px">
                     <v-card>
                         <v-card-title>
                             Add Product
@@ -46,11 +46,11 @@
                             <v-text-field v-model="prodAmount" :items="amount" label="Amount" item-value="text"></v-text-field>
                         </v-card-text>
                         <v-card-actions>
-                            <v-btn @click="dialog2 = false" dark color="indigo">
+                            <v-btn @click="dialog2 = !dialog2" dark color="indigo">
                                 Close
                             </v-btn>
                             <v-spacer></v-spacer>
-                            <v-btn v-on:click="addItem(id), dialog2 = false" class="btn-plus" dark color="indigo">
+                            <v-btn @click="addItem, dialog2 = !dialog2" dark color="indigo">
                                 Add
                             </v-btn>
                         </v-card-actions>
@@ -64,6 +64,25 @@
 <script>
     import uuid from 'uuid';
     import draggable from 'vuedraggable'
+    <div class="list" v-bind:key="card.id" v-for="card in cards">
+        <p> {{card.title}} </p>
+        
+        
+        <!-- Backlog draggable component. Pass arrBackLog to list prop -->
+        <draggable class="list-group kanban-column" :items="items" group="tasks" @change="log">
+            <div class="col" v-bind:key="item.id" v-for="item in items">
+                {{ item.title }} <button @click="$emit('del-item', item.id)" class="del">x</button>
+                
+            </div>
+        </draggable>
+    </div>
+</template>
+
+<script>
+    //import CardsItem from './CardsItem.vue'
+    import {
+        VueDraggableNext
+    } from 'vue-draggable-next'
     export default {
         components: {
             draggable
@@ -72,6 +91,7 @@
         data() {
             return {
                 dialog2: false,
+                widgets: false,
                 productName: [{
                         text: 'Fleece Pants'
                     },
@@ -104,55 +124,63 @@
                     text: 'bordo'
                 }],
                 cards: [{
+                        items: [{
+                            id: 1,
+                            productName: "Fleece Pants",
+                            size: "44-52",
+                            amount: 35,
+                            color: "black"
+                        }],
                         id: 1,
                         title: 'Product List',
-                        flex: 3.
+                        flex: 3
                     },
                     {
+                        items: [{}],
                         id: 2,
                         title: 'In Production',
                         flex: 3
                     },
                     {
+                        items: [{}],
                         id: 3,
                         title: 'In Stock',
                         flex: 3
                     },
                     {
+                        items: [{}],
                         id: 4,
                         title: 'Exported for Sale',
                         flex: 3
                     },
                 ],
-                items: [],
-                amount: '',
                 prodName: '',
                 prodSize: '',
+                prodColor: '',
                 prodAmount: '',
-                prodColor: ''
+                exists: null,
             }
         },
         methods: {
-            deleteItem(number) {
-                // this.items.pop();
-                const index = this.items.findIndex(s => s === number);
-                this.items.splice(index, 1);
-            },
-            addItem(id) {
-                console.log(id);
-                this.items.push({
-                    id: uuid.v4(),
-                    name: this.prodName,
-                    size: this.prodSize,
-                    amount: this.prodAmount,
-                    color: this.prodColor,
-                });
-                this.prodName = "";
-                this.prodSize = "";
-                this.prodAmount = "";
-                this.prodColor = "";
-            },
+            addItem() {
+                if (this.prodName) {
+                    this.cards.item.push({
+                        id: uuid.v4(),
+                        title: this.prodName,
+                        size: this.prodSize,
+                        amount: this.prodAmount,
+                        color: this.prodColor
+                    });
+                }
+            }
         },
+            draggable: VueDraggableNext
+            // CardsItem
+        },
+        props: ["cards", "items"],
+        methods: {
+            
+        }
     }
 </script>
 
@@ -165,6 +193,7 @@
         margin: 5px 5px 5px auto;
     }
     .card-items {
+    .container2 {
         position: relative;
         box-shadow: 1px;
         border-radius: 2px;
@@ -185,3 +214,14 @@
         font-size: 15px;
     }
 </style>
+    .col {
+        width: 200px;
+        background-color: #e3e3e3;
+        border-radius: 4px;
+        padding: 10px;
+        border: 1px solid #999;
+        vertical-align: top;
+        position: relative;
+    }
+</style>
+
